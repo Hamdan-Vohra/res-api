bd861e9fcb203976872e57cccf0acb78e7f4d32f
Hamdan-Vohra <hamdanvohra5676@gmail.com>
2024-06-27 16:10:56 +0500

    Adding CORS handling

--- app.js
+++ app.js
@@ -7,14 +7,29 @@ const orderRoutes = require('./routes/orders')
 //creating express app
 const app = express();
 
+//                  These Middlewares are funnels for each request
+
 app.use(morgan('dev')) //it will log about the requests and it will call next function on its behalf
 
-// to parse the body urlencoded and supports json object but doesn't support files
+// To parse the body urlencoded and supports json object but doesn't support files
 //extended: true to allow the extended bodies with rich data
 //extended: false allows to support only simple bodies fir URL encoded
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-//middleware
+
+//Handling CORS Errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')// '*' is to allow for all route we can specify the route
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT POST GET PATCH DELETE')//all http methods you use in your api for routing
+        return res.status(200).json({})
+    }
+    //if we don't encounter in if condition then no return statement runs so next() is called
+    next();
+})
+
+//middleware for routes
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
